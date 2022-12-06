79f315490e3ad572bda77061e79c2e0576daba06
sheazp <124190139@qq.com>
2022-12-06 22:32:20 +0800

    Create daemon.go

---
+++ daemon.go
@@ -0,0 +1,23 @@
+paclkage daemon
+import (
+ 	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func init() {
+	goDaemon := flag.Bool("d", false, "run app as a daemon")
+	flag.Parse()
+
+	if *goDaemon {
+		cmd := exec.Command(os.Args[0], flag.Args()...)
+
+		if err := cmd.Start(); err != nil {
+			fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
+		os.Exit(0)
+	}
+}
